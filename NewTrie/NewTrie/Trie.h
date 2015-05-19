cdf62bc044efec20b88a1f65d0e3a982d75b2785
setupup <mayforrain@gmail.com>
2015-05-19 17:50:24 +0900

    the latest

--- NewTrie/NewTrie/Trie.h
+++ NewTrie/NewTrie/Trie.h
@@ -8,6 +8,10 @@
 #include <string>
 #include<iostream>
 using namespace std;
+typedef struct mix_data{
+	string value;
+	double coordinate[2];
+}Mixdata;
 template<typename T>
 class trie
 {
@@ -23,60 +27,92 @@ public:
 		return flag && collection == value ? true : false;
 	}
 
-	void insert(const T& collection,const double& first,const double& second) {
+	void insert(const T& collection) {
 		trie *node = this;
 		for (auto &c : collection) {
 			auto found = node->children.find(c); //first part of children is a char second is a trie(subtree) children is a pair set pair<key,value>
 			if (found == node->children.end()) //cannot find
-				node->children[c] = trie(node->value + c); //root node orgin value is ''
+				node->children[c] = trie(node->mixdata.value + c); //root node orgin value is ''
 			node = &(node->children[c]);
 		}
 		node->flag = true;
-		node->d2[0]=first;
-		node->d2[1]=second;
+	}
+	void insert(const T& collection, const double& first, const double& second) { //parameter overload
+		trie *node = this;
+		for (auto &c : collection) {
+			auto found = node->children.find(c); //first part of children is a char second is a trie(subtree) children is a pair set pair<key,value>
+			if (found == node->children.end()) //cannot find
+				node->children[c] = trie(node->mixdata.value + c); //root node orgin value is ''
+			node = &(node->children[c]);
+		}
+		node->flag = true;
+		node->mixdata.coordinate[0] = first;
+		node->mixdata.coordinate[1] = second;
+	}
+	vector<const Mixdata*> singleGeoQuery(vector<const Mixdata*>& data,double upleft[],double downright[]){
+		vector<const Mixdata*> result;
+		for (const auto& lt : data)
+		{
+			if ((*lt).coordinate[0]>upleft[0] && (*lt).coordinate[0]<downright[0] && (*lt).coordinate[1]<upleft[1] && (*lt).coordinate[0]>downright[1])
+			{
+				result.push_back(lt);
+			}
+			
+		}
+		return result;
 	}
 	//list method list all the exist pointers
-	std::vector<const T*> list() const {
-		std::vector<const T*> results;
+	std::vector<const Mixdata*> list() const {
+		std::vector<const Mixdata*> results;
 
 		if (flag)	
-			results.push_back(&value);  //what is the address mean// explanation, as the results defination is pointer *
+			results.push_back(&mixdata);  //what is the address mean// explanation, as the results defination is pointer *
 		//can learn from this
 		for (const auto &it : children) {
 			auto values = it.second.list(); //recursive from the subtree //is this really necessary
+			//merge deepone into this layer
 			//I prefer to write the it.second.list()  //explanation look at line 39
-			for (const auto &value : values)
-				results.push_back(value); //this value is also a pointer
+			for (const auto &mixdata : values)
+				results.push_back(mixdata); //this value is also a pointer
 		}
 
 		return results;
 	}
 	//complete method traverse the rest trie node
-	std::vector<const T *> complete(const T &prefix) const {
+	std::vector<const Mixdata *> complete(const string &prefix) const {
 		const trie *node = this;
 		int i = 0;
 		for (auto &elem : prefix) {
 
 			++i;
 
-			const auto found = node->children.find(elem); //everytime the node changes, iterator
+			const auto found = node->children.find(elem); //this find() on the map should be very effcient
 			const auto found1 = node->children.find('#');
 			if (found == node->children.end()&&found1==node->children.end())
-				return std::vector<const T *>();
+				return std::vector<const Mixdata *>();
 			else if (found == node->children.end() && found1 != node->children.end())
 			{
-				node = &found1->second;
+				node = &(found1->second);
 			}
 			else if (found != node->children.end() && found1 == node->children.end())
 			{
-				node = &found->second;
+				node = &(found->second);
 			}
 			else{
-				const trie *node1 = &found->second;
-				const trie *node2 = &found1->second;
+				const trie *node1 = &found->second; //elem
+				const trie *node2 = &found1->second; //#
 				
-				node1->complete(prefix.substr(i));
-				node2->complete(prefix.substr(i));
+				vector<const Mixdata*> temp1=node1->complete(prefix.substr(i));
+				vector<const Mixdata*> temp2 = node2->complete(prefix.substr(i));
+				//vector<const string*>::const_iterator end1 = temp1.end();
+				vector<const Mixdata*>::const_iterator be2 = temp2.begin();
+				vector<const Mixdata*>::const_iterator end2 = temp2.end();
+				
+				for (; be2 != end2; ++be2)
+				{
+					temp1.push_back(*be2);
+				}
+				return temp1;
 			}
 							
 		}
@@ -84,36 +120,38 @@ public:
 		return node->list();  //print all the answers under the midway node
 	}
 	//return the list size
-	void printlist(vector < const T*>& list)
+	void printlist(vector < const Mixdata*>& list)
 	{
 		for (const auto& it : list)
 		{
-			std::cout << *it << endl;
+			std::cout << (*it).value<<" "<<(*it).coordinate[0]<<","<<(*it).coordinate[1]  << endl;
 		}
 	}
 	unsigned size() const {
 		return list().size();
 	}
-	void generate_DMV(const string& s,fstream& output,const int& threshold=1)
+	void generate_DMV(const string& s,double& first,double& second,fstream& output,const int& threshold=1)
 	{
 
-		output << s << endl;
+		output << s << ' '<<first<<' '<<second<<endl; //the first one string
 		for (int i = 0; i < s.length(); ++i)
 		{
 			string temp = s;
 			temp[i] = '#';
-			output << temp << endl;
+			output << temp << ' ' << first << ' ' << second << endl;
 		}
 	}
 
 protected:
 
-	trie(const T& _value) : value(_value), flag(false)
-	{}
+	trie(const string& _value) : flag(false)
+	{
+		mixdata.value = _value;
+	}
 
-	T value;
+	Mixdata mixdata;
 	bool flag; //represent whether its a leaf node
-	double d2[2];
+	
 	std::map< typename T::value_type, trie > children;
 };
 
--- NewTrie/NewTrie/main.cpp
+++ NewTrie/NewTrie/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-
+//#include "Timer.h"
 #include "trie.h"
+#include <Windows.h>
+
 
 using namespace std;
 int main()
 {
+	double elapsetime;
+	
 	trie<std::string> t;
-	fstream output("DMV1.txt", ios_base::in|ios_base::app);
+	fstream output("finalDMV.txt", ios_base::in|ios_base::app);
 
 	
 	/*t.insert("wargame",1,1);
@@ -16,36 +20,52 @@ int main()
 	t.insert("wolfram",3,3);
 	t.insert("world",4,4);
 	t.insert("work",5,5);*/
-	ifstream input("short.txt");
-	string temp;
-	while (input >> temp)
-	{
-		t.generate_DMV(temp, output);
-	}
+	//generate something according to the origin data into the output
+	//ifstream input("finally.txt"); //origin source
+	//double coor[2];
+	//string temp;
+	//while (input >> temp)
+	//{
+	//	input >> coor[0] >> coor[1];
+	//	t.generate_DMV(temp,coor[0],coor[1], output);
+	//}
 	
 	
 	string dmv;
+	double coordinate[2];
 	output.seekg(0); //
-	while (output >> dmv)
+	double start = GetTickCount();
+	while (output >> dmv) //output format text co1 co2
 	{
-		t.insert(dmv, 1, 1);
+		output >> coordinate[0] >> coordinate[1];
+		t.insert(dmv,coordinate[0],coordinate[1]);
 	}
+	double end = GetTickCount();
+	cout << end-start << endl;
 	//t.printlist(t.complete("wo"));
+	start = GetTickCount();
 	auto list = t.complete("pl");
+	end = GetTickCount();
+	cout << end-start << endl;
+	double left[2] = { 48.0, -125.0 };
+	double right[2] = { 31, -83 };
+	auto result = t.singleGeoQuery(list,left ,right );
 	t.printlist(list);
 	
 	
-	//assert(t.size() == 5);
-	//assert(t.has("war") == false);
-	//assert(t.has("wargame") == true);
-
-	//auto candidates = t.complete("wo");
-	//assert(candidates.size() == 4);
-
-	//candidates = t.complete("warg");
-	//assert(candidates.size() == 1);
-	//assert(*candidates[0] == "wargame");
+	
 
 	std::cout << "All ok." << std::endl;
 	return 0;
 }
+
+//assert(t.size() == 5);
+//assert(t.has("war") == false);
+//assert(t.has("wargame") == true);
+
+//auto candidates = t.complete("wo");
+//assert(candidates.size() == 4);
+
+//candidates = t.complete("warg");
+//assert(candidates.size() == 1);
+//assert(*candidates[0] == "wargame");
